01fdd44ed968db47440df981488318e2c2c6e17a
Francois Sharpe <francois.sharpe@live.com>
2019-10-31 21:10:40 +0100

    Revert "added tests and basic endpoint along with travis ci +tests configuration"

--- springbot/resources/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask_restful import Api
-from springbot.resources.teams.team import TeamCollection, TeamItem
-
-
-api = Api()
-api.add_resource(TeamCollection, '/teams/')
-api.add_resource(TeamItem, '/teams/<int:team_id>')
